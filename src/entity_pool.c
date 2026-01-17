8a488673ba78dd5d169f6c2bb4d1ef51aedc47c1
Thomas Bricout <thomas.bricout@etu.imt-nord-europe.fr>
2026-01-17 23:41:04 +0100

    add component bitmaps and minimal entity destruction

--- src/entity_pool.c
+++ src/entity_pool.c
@@ -10,46 +10,68 @@ void POOL_Init(EntityPool *pool) {
         pool->id[i] = default_id;
         pool->display_rect[i] = default_rect;
         pool->tex_location[i] = TEX_DEBUG;
+
+        pool->display_rect_map[i] = SDL_FALSE;
+        pool->tex_location[i] = SDL_FALSE;
     }
 }
 
 void POOL_Load(EntityPool *pool) {
     // Adds the main player
     SDL_Rect player_display_rect = {100, 100, 100, 100};
-    POOL_New_entity(pool, TEX_DEBUG, player_display_rect);
+    pool->player = POOL_New_entity_classic(pool, TEX_DEBUG, player_display_rect);
 }
 
-EntityID POOL_New_entity(EntityPool *pool, TextureLocation tex_location, SDL_Rect display_rect) {
+EntityID POOL_New_entity_classic(EntityPool *pool, TextureLocation tex_location, SDL_Rect display_rect) {
     // Create new entity with new a new unique id and a designated position in the pool
     static int currentID = 1;
     EntityID new_id;
 
     new_id.location = pool->currentCount; // TODO allow reusage of freed up places
-    new_id.id = currentID;
+    new_id.unique_id = currentID;
     currentID ++;
 
-    SDL_Log("Creating new Entity with location %d and id %d", new_id.location, new_id.id);
+    SDL_Log("Creating new Entity with location %d and unique_id %d", new_id.location, new_id.unique_id);
 
     pool->id[new_id.location] = new_id;
     pool->tex_location[new_id.location] = tex_location;
     pool->display_rect[new_id.location] = display_rect;
+    pool->tex_location_map[new_id.location] = SDL_TRUE;
+    pool->display_rect_map[new_id.location] = SDL_TRUE;
 
     pool->currentCount ++;
 
     return new_id;
 }
 
+void POOL_Destroy_entity(EntityPool *pool, EntityID id) {
+    // Check if entity has not yet been destroyed
+    if (pool->id[id.location].unique_id != id.unique_id) {
+        return;
+    }
+
+    pool->currentCount --;
+    pool->id[id.location] = (EntityID) {0, 0};
+
+    // Reset all bitmaps
+    pool->tex_location_map[id.location] = SDL_FALSE;
+    pool->display_rect_map[id.location] = SDL_FALSE;
+}
+
 void POOL_Display_All(AssetManager *assetManager, EntityPool *pool, SDL_Renderer *renderer) {
     SDL_LogDebug(SDL_LOG_CATEGORY_RENDER, "Starting displaying entities");
 
     for (int i = 0; i < pool->currentCount; i++) {
+        // Skip entities that don't have texture / display rect
+        if (!pool->display_rect_map[i] || !pool->tex_location_map[i]) { continue; }
+
         SDL_Texture *tex = assetManager->asset_array[pool->tex_location[i]];
         SDL_RenderCopy(renderer, tex, NULL, &pool->display_rect[i]);
 
         SDL_LogDebug(
             SDL_LOG_CATEGORY_RENDER,
-            "Rendering Entity with location %d, id %d, texture %d, coordinates (%d, %d)",
-             i, pool->id[i].id, pool->tex_location[i], pool->display_rect[i].x, pool->display_rect[i].y
+            "Rendering Entity with location %d, unique_id %d, texture %d, coordinates (%d, %d)",
+             i, pool->id[i].unique_id, pool->tex_location[i], pool->display_rect[i].x, pool->display_rect[i].y
         );
     }
 }
--- src/entity_pool.h
+++ src/entity_pool.h
@@ -8,20 +8,31 @@
 
 typedef struct {
     int location;
-    int id;
+    int unique_id;
 } EntityID;
 
 typedef struct {
+    // Keeping track of which spots are occupied by valid entities
     int currentCount;
+    SDL_bool emptyLocations[MAX_ENTITY_COUNT];
 
+    // Keep track of specific entities or entity lists ( for example, all entities which might damage the player )
+    EntityID player;
+
+    // Each list contains a specific component for all entities in the game
     EntityID id[MAX_ENTITY_COUNT];
     TextureLocation tex_location[MAX_ENTITY_COUNT];
     SDL_Rect display_rect[MAX_ENTITY_COUNT];
+
+    // Bitmaps ( whether a specific entity has a component )
+    SDL_bool tex_location_map[MAX_ENTITY_COUNT];
+    SDL_bool display_rect_map[MAX_ENTITY_COUNT];
 } EntityPool ;
 
 void POOL_Init(EntityPool *pool);
 void POOL_Load(EntityPool *pool);
-EntityID POOL_New_entity(EntityPool *pool, TextureLocation tex_location, SDL_Rect display_rect);
+EntityID POOL_New_entity_classic(EntityPool *pool, TextureLocation tex_location, SDL_Rect display_rect);
+void POOL_Destroy_entity(EntityPool *pool, EntityID id);
 void POOL_Display_All(AssetManager *assetManager, EntityPool *pool, SDL_Renderer *renderer);
 
 #endif
