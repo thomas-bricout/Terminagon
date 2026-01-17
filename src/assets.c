2944fd05bdfae805ce9c0557af325afee5aac299
Thomas Bricout <thomas.bricout@etu.imt-nord-europe.fr>
2026-01-17 09:43:17 +0100

    define enum TextureLocation

--- src/assets.c
+++ src/assets.c
@@ -3,6 +3,8 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
+#include <assert.h>
+
 void ASSETS_Init(AssetManager *assetManager, SDL_Renderer *renderer) {
     SDL_Texture *debugTexture = IMG_LoadTexture(renderer, "debug.png");
     for ( int i = 0; i < ASSET_ARRAY_LENGTH; i++ ) {
@@ -16,4 +18,9 @@ void ASSETS_Load(AssetManager *assetManager, SDL_Renderer *renderer) {
 
 void ASSETS_Destroy(AssetManager *assetManager) {
     SDL_DestroyTexture(assetManager->asset_table);
+}
+
+SDL_Texture *ASSETS_AccessTexture(AssetManager *assetManager, TextureLocation textureLocation) {
+    assert(textureLocation < ASSET_ARRAY_LENGTH);
+    return assetManager->asset_array[textureLocation];
 }
--- src/assets.h
+++ src/assets.h
@@ -5,6 +5,10 @@
 
 #define ASSET_ARRAY_LENGTH 100
 
+typedef enum {
+    debug = 0
+} TextureLocation ;
+
 typedef struct {
     SDL_Texture *asset_table;
     SDL_Texture *asset_array[ASSET_ARRAY_LENGTH];
@@ -13,5 +17,6 @@ typedef struct {
 void ASSETS_Init(AssetManager *assetManager, SDL_Renderer *renderer);
 void ASSETS_Load(AssetManager *assetManager, SDL_Renderer *renderer);
 void ASSETS_Destroy(AssetManager *assetManager);
+SDL_Texture *ASSETS_AccessTexture(AssetManager *assetManager, TextureLocation textureLocation);
 
 #endif
